d66e0c0d6b5ffa3a479793eab6a579c754516f1f
kingtimm <kingtimm@gmail.com>
2023-08-09 20:40:18 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <OgImage
+    title="Hey!"
+    description="Welcome to the Nuxt OG Image demo. Try changing this text."
+    icon="carbon:help"
+  />
+  <h1>index page</h1>
+  <div>Playground preview: available at /__og_image__</div>
+  <iframe src="/__og_image__" style="width: 100%; border: none; height: 800px">
+  </iframe>
+</template>
